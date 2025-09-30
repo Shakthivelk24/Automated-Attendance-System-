ceba7a6a7d48437bf2a304b22fa8e5f636fdb031
Shakthivelk24 <shakthivelk1124@gmail.com>
2025-09-30 21:53:32 +0530

    Added Router for all Compontent

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from "./pages/LandingPage"
 import Auth from "./pages/Auth"
 import GovDashboard from "./pages/GovDashboard"
+import TeacherDashboard from "./pages/TD1"
+import TD2 from "./pages/TD2"
+import TD3 from "./pages/TD3"
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/sign" element={<Auth />} />
           <Route path="/dashboard" element={<GovDashboard />} />
+          <Route path="/TD1" element={<TeacherDashboard/>} />
+          <Route path="/TD2" element={<TD2/>} />
+          <Route path="/TD3" element={<TD3/>} />
         </Routes>
       </Router>
     </>
