ad841ea3a63ee05441f7965f66e09da6391c73b6
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-02-06 21:23:26 +0530

    refactor: update App component to use Routes for navigation and remove commented code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import LandingPage from "./pages/LandingPage"
-import Auth from "./pages/Auth"
-import GovDashboard from "./pages/GovDashboard"
-import TeacherDashboard from "./pages/TD1"
-import TD2 from "./pages/TD2"
-import TD3 from "./pages/TD3"
-/*
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import GovDashboard from "./pages/GovDashboard";
+import TeacherDashboard from "./pages/TD1";
+import TD2 from "./pages/TD2";
+import TD3 from "./pages/TD3";
+import SchoolAuth from "../Authentication/SchoolAuth";
+import GovernmentAuth from "../Authentication/GovernmentAuth";
+
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/sign" element={<Auth />} />
-          <Route path="/dashboard" element={<GovDashboard />} />
-          <Route path="/TD1" element={<TeacherDashboard/>} />
-          <Route path="/TD2" element={<TD2/>} />
-          <Route path="/TD3" element={<TD3/>} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/schoolAuth" element={<SchoolAuth />} />
+        <Route path="/governmentAuth" element={<GovernmentAuth />} />
+        <Route path="/gov-dashboard" element={<GovDashboard />} />
+        <Route path="/teacher-dashboard" element={<TeacherDashboard />} />
+        <Route path="/teacher-dashboard/td2" element={<TD2 />} />
+        <Route path="/teacher-dashboard/td3" element={<TD3 />} />
+      </Routes>
     </>
-  )
-}*/
-function App() {
-  return (
-    <>
-      <LandingPage />
-      <Auth />
-      <GovDashboard />
-      <TeacherDashboard/>
-      <TD2/>
-      <TD3/>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
