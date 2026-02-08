152d048f853bf414271c4580ca723047ca2e7d57
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-02-08 22:11:49 +0530

    feat: set up Express server with CORS and teacher route integration

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import connectDB from './config/db.js';
 import 'dotenv/config';
+import teacher from './routes/teacher.route.js';
+import cors from 'cors';
+
 
 connectDB();
 
@@ -11,6 +14,10 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+app.use(express.json());
+app.use(cors());
+app.use("/api/teacher",teacher);
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
